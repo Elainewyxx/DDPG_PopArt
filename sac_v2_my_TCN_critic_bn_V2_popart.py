0cc42f4f9769dabcad8dc52685525eb6dd17fa04
taomo <taomo1220@sina.com>
2019-10-24 00:51:06 +0800

    Update sac_v2_my_TCN_critic_bn_V2_popart.py

--- sac_v2_my_TCN_critic_bn_V2_popart.py
+++ sac_v2_my_TCN_critic_bn_V2_popart.py
@@ -504,12 +504,12 @@ max_episodes  = 10000
 # max_steps   = 20 if ENV ==  'Reacher' else 150  # Pendulum needs 150 steps per episode to learn well, cannot handle 20
 max_steps = 150   #150
 frame_idx   = 0
-batch_size  = 256  #256
+batch_size  = 64  #256
 explore_steps = 200  # for random action sampling in the beginning of training
 update_itr = 1
 AUTO_ENTROPY=True
 DETERMINISTIC=False
-hidden_dim = 64  # 512
+hidden_dim = 32  # 512
 rewards     = []
 # model_path = './model'
 model_path = './model_popart'
